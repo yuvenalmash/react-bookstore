d10a17ee9116a1ec1f4e0f62bf5f97d24620fcd4
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-01-20 01:22:56 +0300

    create routes to pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Layout from './pages/Layout';
+import Books from './pages/Books';
+import Categories from './pages/Categories';
+import NoPage from './pages/NoPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Bookstore</p>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Books />} />
+          <Route path="categories" element={<Categories />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </>
+);
 
 export default App;
