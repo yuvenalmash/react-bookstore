c10607ce1051305575cb1269f46072d7f6673d5d
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-01-24 18:42:55 +0300

    implement ducks pattern

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,27 +1,12 @@
-// actions
-let nextBookId = 2;
-export const addBook = (title, author, genre) => {
-  nextBookId += 1;
-  return (
-    {
-      type: 'ADD_BOOK',
-      id: toString(nextBookId),
-      title,
-      author,
-      genre,
-    });
-};
-
-export const removeBook = () => ({
-  type: 'REMOVE_BOOK',
-});
+const ADD_BOOK = 'ADD_BOOK';
+const REMOVE_BOOK = 'REMOVE_BOOK';
 
 // reducer
-export const initialState = [];
+const initialState = [];
 
 const bookReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'ADD_BOOK':
+    case ADD_BOOK:
       return [
         ...state,
         {
@@ -33,13 +18,29 @@ const bookReducer = (state = initialState, action) => {
           currentChapter: '0',
         },
       ];
-    case 'REMOVE_BOOK':
-      return [
-        ...state,
-      ];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.id);
     default:
       return state;
   }
 };
-
 export default bookReducer;
+
+// actions
+let nextBookId = 2;
+export const addBook = (title, author, genre) => {
+  nextBookId += 1;
+  return (
+    {
+      type: ADD_BOOK,
+      id: toString(nextBookId),
+      title,
+      author,
+      genre,
+    });
+};
+
+export const removeBook = (id) => ({
+  type: REMOVE_BOOK,
+  id,
+});
--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,18 +1,19 @@
-// actions
-export const checkStatus = () => ({
-  type: 'CHECK_STATUS',
-});
+const CHECK_STATUS = 'CHECK_STATUS';
 
 // reducer
 const initialState = [{ status: 'UNDER CONSTRUCTION' }];
 
 const categoryReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'CHECK_STATUS':
+    case CHECK_STATUS:
       return state[0].status;
     default:
       return state;
   }
 };
-
 export default categoryReducer;
+
+// actions
+export const checkStatus = () => ({
+  type: CHECK_STATUS,
+});
