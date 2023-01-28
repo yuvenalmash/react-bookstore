6244b765fd0a1dce3e46f17def2da7ca0b2828bd
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-01-28 22:32:07 +0300

    Style 'page not found'

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,3 +1,10 @@
-const NoPage = () => <h1>404</h1>;
+import styles from './css/Nopage.module.scss';
+
+const NoPage = () => (
+  <div className={styles.container}>
+    <h1>404</h1>
+    <p>Page Not Found!</p>
+  </div>
+);
 
 export default NoPage;
